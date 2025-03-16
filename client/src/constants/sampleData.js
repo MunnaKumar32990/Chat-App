05b6d622f5ccd90174f9f6ff2106987acd075db0
MunnaKumar32990 <2300032990@kluniversity.in>
2025-03-16 23:50:21 +0530

    Added NewGroup and Notifications components with user selection and friend request handling features. Improved UI alignment and state management.

--- client/src/constants/sampleData.js
+++ client/src/constants/sampleData.js
@@ -18,3 +18,35 @@ members:["1","2"],
 
 }
 ]
+
+export const sampleUsers =[{
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "John Doe",
+    _id: "1",
+   
+}
+,
+{
+    avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: " Doe",
+    _id: "2",
+}
+]
+
+export const sampleNotifications =[{
+    sender:{
+        avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: "John Doe",
+    },
+    _id: "1",
+   
+}
+,
+{
+   sender:{
+     avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+    name: " Doe"
+   },
+    _id: "2",
+}
+]
